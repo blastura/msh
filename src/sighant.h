2a71245ca113b9dd6856bc4f66f4e7404d4d91a2
Anton Johansson <dit06ajn@peppar.cs.umu.se>
2009-05-12 22:56:29 +0200

    Fixed compiling error on solaris

--- src/sighant.h
+++ src/sighant.h
@@ -1,6 +1,14 @@
 /* Handles singals */
 void signal_handler(int signum);
 
+/* Needed for compiling sighant.c  on solaris, code from www.apuebook.com */
+#if defined(SOLARIS)
+#define _XOPEN_SOURCE	500	/* Single UNIX Specification, Version 2  for Solaris 9 */
+#define CMSG_LEN(x)	_CMSG_DATA_ALIGN(sizeof(struct cmsghdr)+(x))
+#elif !defined(BSD)
+#define _XOPEN_SOURCE	600	/* Single UNIX Specification, Version 3 */
+#endif
+
 /* Error safe signal handling from http://www.apuebook.com */
 typedef	void	Sigfunc(int);	/* for signal handlers */
 Sigfunc* signal(int signo, Sigfunc *func);
